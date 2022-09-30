a75e45713ceaa967e8bf12171a2ebec6d00b1f4a
kleber.kaerk <apenaskleber123@gmail.com>
2022-09-30 14:21:55 -0300

    Criação do projeto

---
+++ src/main/java/com/webservicepizzariazeff/www/WebServicePizzariaZeffApplication.java
@@ -0,0 +1,13 @@
+package com.webservicepizzariazeff.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicePizzariaZeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicePizzariaZeffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservicepizzariazeff/www/WebServicePizzariaZeffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicepizzariazeff.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicePizzariaZeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
