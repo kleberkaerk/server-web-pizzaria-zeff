69af30b61c937c14226650c06c2c20050ffbe9f0
kleber.kaerk <apenaskleber123@gmail.com>
2022-11-08 15:07:34 -0300

    Implementação de configuração de CORS

--- src/main/java/com/webservicepizzariazeff/www/config/WebConfig.java
+++ src/main/java/com/webservicepizzariazeff/www/config/WebConfig.java
@@ -1,7 +1,13 @@
 package com.webservicepizzariazeff.www.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +15,8 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final List<String> allowedMethods = List.of("GET", "PUT", "DELETE", "POST");
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -18,5 +26,29 @@ public class WebConfig implements WebMvcConfigurer {
         pageableHandler.setMaxPageSize(10);
 
         resolvers.add(0, pageableHandler);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+
+        corsConfiguration.setAllowedMethods(allowedMethods);
+
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>(new CorsFilter(urlBasedCorsConfigurationSource));
+
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return filterRegistrationBean;
     }
 }
